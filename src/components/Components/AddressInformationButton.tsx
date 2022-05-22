f323001e0a252d66b371c2db65be97d3a91f91f4
0xD4V1NC1 <0xD4V1NC1@gmail.com>
2022-05-22 18:19:58 -0400

    add web3

--- src/components/Components/AddressInformationButton.tsx
+++ src/components/Components/AddressInformationButton.tsx
@@ -6,8 +6,8 @@ const AddressInformationButton = ({address, connector}: {address:string, connect
   if (!address) return null;
   return (
     <div className=''>
-      <div id='connect-wallet-gradient-border' className='p-0.5 bg-gradient-to-br from-primary-400 via-primary-500 to-secondary-500 rounded hover:bg-rainbow hover:animate-rainbow' >
-        <Button type='button' ariaLabel='Click to disconnect wallet' color='primary-gradient-outline' text={`${formatWalletAddress(address, 10)}`} className='py-2 px-6 text-ellipsis overflow-hidden' onClick={() => connector?.deactivate()}/>
+      <div id='address-information-gradient-border' className='p-0.5 bg-gradient-to-br from-primary-400 via-primary-500 to-secondary-500 rounded hover:bg-rainbow hover:animate-rainbow' >
+        <Button type='button' ariaLabel='Click to disconnect wallet' color='primary-gradient-outline' text={`${formatWalletAddress(address, 10)}`} className='py-2 px-6' onClick={() => connector?.deactivate()}/>
       </div>
     </div>
   );
