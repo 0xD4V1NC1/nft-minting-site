dc017be59ba7172d9a2250ac334425cc83588765
0xD4V1NC1 <97196034+0xD4V1NC1@users.noreply.github.com>
2022-07-17 13:44:24 -0400

    feat: unify hardhat and react-app (#12)
    * unify hardhat and react-app
    
    * update gitignore
    
    * remove accidental uploaded files
    
    * remove files
    
    * update git ignore again

---
+++ hardhat.config.ts
@@ -0,0 +1,70 @@
+import * as dotenv from 'dotenv';
+
+import {HardhatUserConfig, task} from 'hardhat/config';
+
+import '@nomiclabs/hardhat-etherscan';
+
+import '@nomiclabs/hardhat-waffle';
+
+import '@typechain/hardhat';
+
+import 'hardhat-gas-reporter';
+
+import 'solidity-coverage';
+
+
+dotenv.config();
+
+/*
+  This is a sample Hardhat task. To learn how to create your own go to https://hardhat.org/guides/create-task.html
+  To run this task type npx hardhat accounts in the terminal
+*/
+task('accounts', 'Prints the list of accounts', async (_taskArgs:any, hre: any) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    const balance = await account.provider?.getBalance(account.address);
+    console.log(`Address: ${account.address}`);
+    console.log(`Balance: ${parseFloat(hre.ethers.utils.formatEther(balance||0))}`);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+const config: HardhatUserConfig = {
+  solidity: '0.8.4',
+  defaultNetwork: 'localhost',
+  networks: {
+    localhost: {
+      url: `http://127.0.0.1:8545/`,
+      // accounts: [`0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80`],
+      // accounts: process.env.REACT_APP_PRIVATE_KEY !== undefined ? [process.env.REACT_APP_PRIVATE_KEY] : [],
+    },
+    ropsten: {
+      url: process.env.REACT_APP_ROPSTEN_URL || '',
+      accounts:
+        process.env.REACT_APP_PRIVATE_KEY !== undefined ? [process.env.REACT_APP_PRIVATE_KEY] : [],
+    },
+    mumbai: {
+      url: `https://speedy-nodes-nyc.moralis.io/${process.env.REACT_APP_NODE_MORALIS_API_KEY}/polygon/mumbai`,
+      accounts: process.env.REACT_APP_PRIVATE_KEY !== undefined ? [process.env.REACT_APP_PRIVATE_KEY] : [],
+    },
+    matic: {
+      url: `https://speedy-nodes-nyc.moralis.io/${process.env.REACT_APP_NODE_MORALIS_API_KEY}/polygon/mainnet`,
+      accounts: process.env.REACT_APP_PRIVATE_KEY !== undefined ? [process.env.REACT_APP_PRIVATE_KEY] : [],
+    },
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: 'USD',
+  },
+  etherscan: {
+    apiKey: process.env.REACT_APP_POLYSCAN_API_KEY,
+  },
+};
+
+export default config;
+// module.exports = {
+//   config,
+// };
